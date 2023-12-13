a63bb86a59667be64b59754e0bd8ac0774bdac5e
Anana142 <a1naanana@mail.ru>
2023-12-13 12:08:23 +1000

    Добавьте файлы проекта.

---
+++ VM/RegistrationVM.cs
@@ -0,0 +1,53 @@
+﻿using SeaBattleWPF.API.Game;
+using SeaBattleWPF.mvvm;
+using SeaBattleWPF.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SeaBattleWPF.VM
+{
+    public class RegistrationVM : BaseVM
+    {
+        public string LoginText { get; set; }
+        private PasswordBox pass;
+
+        public CommandVM Back { get; set; }
+
+        public CommandVM Registration { get; set; }
+
+        public RegistrationVM()
+        {
+            Registration = new CommandVM(() =>
+            {
+                if (User.TryRegister(LoginText, pass.Password))
+                {
+                    MessageBox.Show("Вы зарегистрированы!");
+                    Back.Execute(new ());
+                }
+                    
+                else
+                    MessageBox.Show("Вы не зарегистрированы(");
+
+               
+            });
+
+            Back = new CommandVM(() =>
+            {
+                PageControl.GetInstance().CurrentPage = new LoginPage();
+            });
+
+
+        }
+
+        public void RegisterPasswordBox(PasswordBox passwordBox) => pass = passwordBox;
+
+
+
+
+    }
+}
---
+++ View/RegistrationPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using SeaBattleWPF.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeaBattleWPF.View
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+
+            var vm = (RegistrationVM)DataContext;
+
+            vm.RegisterPasswordBox(passwordBox);
+        }
+    }
+}
